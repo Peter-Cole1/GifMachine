b0162eb500422c188d68d557b40cdf9d253c76ce
Peter Cole <Peter_Cole1@yahoo.com>
2019-07-23 02:21:11 -0400

    psuedocode

---
+++ assets/js/logic.js
@@ -0,0 +1,9 @@
+//gif creator, use giphy to populate web page with a given word for a specific theme.
+//remember api parameters like q, limit, and rating
+//create an array of strings , each related to a topic i like. save to a variable called 'topics'.
+//take the topics and create buttons in the html
+//use loop to append buttons for each string 
+//when button is clicked, grab 10 gifs (non-animated) and place on page
+//on click, the gif should animate. click again to stop
+//display rating of gif under each respective gif
+//add a form to add a value to the topics array. make function call that takes each topic and remakes the buttons on the page.
